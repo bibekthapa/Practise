f702670fdebfab289284f69c28bc4d61c93a6a5b
Bibek <bibek.thapahere@gmail.com>
2016-10-24 09:07:44 -0700

    Exercise 2.3 making name to uppercase

--- src/com/example/Practise/Program.java
+++ src/com/example/Practise/Program.java
@@ -1,16 +1,17 @@
 package com.example.Practise;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
+              String name;
             
-            int a=(int) (Math.random()*6+1);
-            int b=(int) (Math.random()*6+1);
+            Scanner input=new Scanner(System.in);
+            System.out.println("Enter the name");
+            name=input.next();
+            System.out.println("Hello, "+name.toUpperCase()+" ,nice to meet you");
             
-            System.out.println("The first die comes up is " +a);
-           
-            System.out.println("The second die comes up is "+b);
-            System.out.println("The total roll is "+(a+b));
 
     }
     
