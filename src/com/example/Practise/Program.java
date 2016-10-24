20abfc4280050928c7b36dcfbe5c24dafc733328
Bibek <bibek.thapahere@gmail.com>
2016-10-24 09:02:37 -0700

    Die roll, first and second roll and their addition

---
+++ src/com/example/Practise/Program.java
@@ -0,0 +1,17 @@
+package com.example.Practise;
+
+public class Program {
+
+    public static void main(String[] args) {
+            
+            int a=(int) (Math.random()*6+1);
+            int b=(int) (Math.random()*6+1);
+            
+            System.out.println("The first die comes up is " +a);
+           
+            System.out.println("The second die comes up is "+b);
+            System.out.println("The total roll is "+(a+b));
+
+    }
+    
+}
