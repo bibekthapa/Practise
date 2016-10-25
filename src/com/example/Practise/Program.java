dfd7d21fedf57e18f742e75c6944933f7e16a393
Bibek <bibek.thapahere@gmail.com>
2016-10-25 02:57:26 -0700

    Gross, dozens

--- src/com/example/Practise/Program.java
+++ src/com/example/Practise/Program.java
@@ -5,12 +5,27 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-              String name;
+            int numberOfEggs;
+            int gross = 0;
+            int rem=0;
+            int dozens=0;
+            int finalrem=0;
             
             Scanner input=new Scanner(System.in);
-            System.out.println("Enter the name");
-            name=input.next();
-            System.out.println("Hello, "+name.toUpperCase()+" ,nice to meet you");
+            System.out.println("Enter the number of Eggs");
+            numberOfEggs=input.nextInt();
+            if(numberOfEggs>144)
+            {
+                gross=numberOfEggs/144;
+                rem=numberOfEggs-(gross*144);
+                if(rem>12)
+                {
+                     dozens = rem/12;
+                     finalrem=rem-dozens*12;
+                }
+            }
+       
+            System.out.println("Your number of eggs is "+ gross+" gross,"+dozens+" dozens and "+ finalrem);
             
 
     }
